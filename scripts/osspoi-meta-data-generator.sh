93b10284f54b47b4514cbae9542f07a7f525245e
asa1997 <arunsureshampadath@gmail.com>
2023-03-22 12:41:10 +0530

    Corrected syntax

--- scripts/osspoi-meta-data-generator.sh
+++ scripts/osspoi-meta-data-generator.sh
@@ -55,7 +55,7 @@ function __acc_check_dir_exist
 {
     local dirs=($BES_OSSPOI_DIR $ACC_ROOT_DIR $DATASTORE_DIR)
     local flag=0
-    for dir in $dirs
+    for dir in ${dirs[@]}
     do
         if [[ ! -d $dir ]]; then
 
