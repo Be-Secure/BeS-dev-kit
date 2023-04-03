a2f457ff3027e7955f358c236cd22f4ebf475407
Sudhir Verma <sudhirvvermaa@gmail.com>
2023-04-04 00:36:07 +0530

    Fix codeQL

--- src/codeQL/codeql_report.py
+++ src/codeQL/codeql_report.py
@@ -22,12 +22,15 @@ class CodeQl():
             f'https://api.github.com/repos/Be-Secure/{projectName}/code-scanning/alerts'
         ], stdout=subprocess.PIPE)
         if (result.stdout):
+            result = json.loads(result.stdout)
             version = osspoiVersionDetails.getVersion(bes_id)
             codeQLPath = os.path.join(besecureAssessment, projectName, version, "sast", f"{projectName}-{version}-codeql-report.json")
+            if not os.path.exists(os.path.join(besecureAssessment, projectName, version, "sast")):
+                os.makedirs(os.path.join(besecureAssessment, projectName, version, "sast"))
             fileWrite = open(codeQLPath, "w+", encoding='utf-8')
-            json.dump({}, fileWrite, indent=2)
+            json.dump(result, fileWrite, indent=2)
             fileWrite.close()
-        if result.stderr:
+        else:
             return f"Fail to get the codeQl report {result.stderr}"
 
         
