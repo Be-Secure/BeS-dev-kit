cc924dbeb642610332dda13ece57652348460eed
Sudhir Verma <sudhirvvermaa@gmail.com>
2023-04-24 17:26:06 +0530

    code refactoring and fixing pylint

---
+++ besecure_developer_toolkit/src/create_ossp_master.py
@@ -0,0 +1,208 @@
+"""
+    licence part todo
+"""
+import sys
+import os
+import json
+from urllib.request import urlopen
+from rich import print
+
+
+class OSSPMaster():
+    """
+        Generate OSSPMaster report.
+    """
+
+    def __init__(self, issue_id: int, name: str) -> None:
+        self.issue_id = issue_id
+        self.name = name
+
+    def check_issue_exists(self, issue_id) -> None:
+        """
+            Check for issue id is exits or not.
+        """
+        try:
+            urlopen('https://github.com/Be-Secure/Be-Secure/issues/'+str(issue_id))
+        except Exception as err:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
+            print(
+                f"Could not find issue with id: {str(issue_id)}, error: {str(err)}")
+            sys.exit()
+
+    def check_issue_related_to_project(self):
+        """
+            Check project name from issue
+        """
+        json_data = json.loads(urlopen(
+            f'https://api.github.com/repos/Be-Secure/Be-Secure/issues/{self.issue_id}').read())
+        issue_title = json_data["title"]
+        project_name = str(str(issue_title).split(":")[1]).replace(" ", "")
+        if project_name != self.name:
+            print(
+                f"[bold red]Alert! [yellow]Mismatch issue_id-project : [green] \
+                    Issue id {self.issue_id} does not match the project {self.name}")
+            sys.exit()
+
+    def check_repo_exists(self, name) -> None:
+        """
+            Check if repo exists in Be-Secure or not 
+        """
+        try:
+            urlopen('https://api.github.com/repos/Be-Secure/'+name)
+        except Exception as err:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
+            print(f"Could not find {name} under Be-Secure, Error: {err}")
+            sys.exit()
+
+    def write_tech_stack(self, bes_id):
+        """
+            Check the tech Stack of the Project
+        """
+        raw_data = urlopen(
+            "https://api.github.com/repos/Be-Secure/Be-Secure/issues/"+str(bes_id))
+
+        data = json.loads(raw_data.read())
+
+        body_data = iter(data["body"].splitlines())
+        found = "false"
+        for i in body_data:
+            if i == "### Tech Stack":
+                found = "true"
+                continue
+            if len(i.strip()) == 0:
+                continue
+            if len(i.strip()) != 0 and found == "true":
+                s = str(i.split(" [")[1])
+                s = str(s.split("]")[0])
+                break
+        return s
+
+    def write_project_repos_data(self, project_data):
+        """
+            Create json report for repository reletated content
+            like: main_github_url, main_bes_url etc...
+        """
+        project_repos = {
+            "main_github_url": "",
+            "main_bes_url": "",
+            "all_projects": [
+                {
+                    "id": 0,
+                    "name": "",
+                    "url": ""
+                }
+            ],
+            "all_bes_repos": [
+                {
+                    "id": 0,
+                    "name": "",
+                    "url": ""
+                }
+
+            ]
+        }
+        project_repos.update(
+            {"main_github_url": project_data["parent"]["html_url"]})
+        project_repos.update({"main_bes_url": project_data["html_url"]})
+        project_repos["all_projects"][0]["id"] = project_data["parent"]["id"]
+        project_repos["all_projects"][0]["name"] = project_data["parent"]["full_name"]
+        project_repos["all_projects"][0]["url"] = project_data["parent"]["html_url"]
+        project_repos["all_bes_repos"][0]["id"] = project_data["id"]
+        project_repos["all_bes_repos"][0]["name"] = project_data["full_name"]
+        project_repos["all_bes_repos"][0]["url"] = project_data["html_url"]
+        return project_repos
+
+    def write_tags(self, bes_id):
+        """
+            get the tags from github labels
+        """
+        url = 'https://api.github.com/repos/Be-Secure/Be-Secure/issues/' + \
+            str(bes_id)+'/labels'
+        tags_json_data = urlopen(url)
+        tags_dict = json.loads(tags_json_data.read())
+        tags = []
+        for name in tags_dict:
+            # todo: fixme
+            tags.append(name)
+        return tags
+
+    def write_languages(self, name):
+        """
+            fetch the languages from github URL
+        """
+        raw_data = urlopen(
+            "https://api.github.com/repos/Be-Secure/"+name+"/languages")
+        data = json.loads(raw_data.read())
+        return data
+
+    def write_to_ossp_master(self, f, ossp_master_json, data, overwrite: bool):
+        """
+            Add or override the project details in ossp_master
+        """
+        if overwrite:
+            for i in range(len(ossp_master_json["items"])):
+                if ossp_master_json["items"][i]["id"] == self.issue_id:
+                    ossp_master_json["items"][i] = data
+                    break
+        else:
+            ossp_master_json["items"].append(data)
+        f.seek(0)
+        f.write(json.dumps(ossp_master_json, indent=4))
+        f.truncate()
+
+    def generate_ossp_master(self, overwrite: bool):
+        """
+            Generate ossp master json report
+        """
+        self.check_issue_exists(self.issue_id)
+        self.check_repo_exists(self.name)
+        self.check_issue_related_to_project()
+        osspoi_dir = os.environ['OSSPOI_DIR']
+        write_flag = True
+        f = open(f"{osspoi_dir}/OSSP-Master.json", "r+", encoding="utf-8")
+        ossp_master_json = json.load(f)
+        if not overwrite:
+            for i in range(len(ossp_master_json["items"])):
+                if ossp_master_json["items"][i]["id"] == self.issue_id:
+                    print("[bold red]Alert! [green]Entry for "+str(self.issue_id) +
+                          "-"+self.name+" already present under OSSP-Master.json")
+                    write_flag = False
+                    break
+                else:
+                    write_flag = True
+        if write_flag:
+            url_data = urlopen(
+                f'https://api.github.com/repos/Be-Secure/{self.name}')
+            project_data = json.loads(url_data.read())
+            ossp_data = json.loads('{}')
+            repo_keys = ["id", "bes_tracking_id", "issue_url", "name",
+                          "full_name", "description", "bes_technology_stack",
+                          "watchers_count", "forks_count", "stargazers_count",
+                          "size", "open_issues", "created_at", "updated_at",
+                          "pushed_at", "git_url", "clone_url", "html_url",
+                          "homepage", "owner", "project_repos", "license",
+                          "language", "tags"
+                        ]
+            for i in repo_keys:
+                if i == "id" or i == "bes_tracking_id":
+                    ossp_data[i] = self.issue_id
+                elif i == "issue_url":
+                    ossp_data[i] = 'https://github.com/Be-Secure/Be-Secure/issues/' + \
+                        str(self.issue_id)
+                elif i == "bes_technology_stack":
+                    ossp_data[i] = self.write_tech_stack(self.issue_id)
+                elif i == "project_repos":
+                    ossp_data[i] = self.write_project_repos_data(project_data)
+                elif i == "tags":
+                    ossp_data[i] = self.write_tags(self.issue_id)
+                elif i == "language":
+                    ossp_data[i] = self.write_languages(self.name)
+                else:
+                    ossp_data[i] = project_data[i]
+            self.write_to_ossp_master(
+                f, ossp_master_json, ossp_data, overwrite)
+            f.close()
---
+++ besecure_developer_toolkit/src/generate_report.py
@@ -0,0 +1,133 @@
+"""
+    licence part todo
+"""
+import json
+import sys
+import os
+from urllib.request import urlopen
+from rich import print
+
+
+class Report():
+    """
+        Create report for scorcard, codeQl, and criticality_score
+    """
+    def __init__(self, issue_id: int, name: str, version: str, report: str):
+        self.issue_id = issue_id
+        self.name = name
+        self.report = report
+        self.version = version
+
+    def fetch_report(self, url):
+        """
+            create json report for criticality_score, codeql, and scorecard
+        """
+        if self.report == "criticality_score":
+            os.system('criticality_score --repo '+url +
+                      ' --format json >> /tmp/'+self.report+'.json')
+            f_critical = open('/tmp/'+self.report+'.json', 'r', encoding="utf-8")
+            data = json.load(f_critical)
+            f_critical.close()
+
+        elif self.report == "codeql":
+            token = os.environ['GITHUB_AUTH_TOKEN']
+            cmd = 'curl -s -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ' + \
+                token+'" -H "X-GitHub-Api-Version: 2022-11-28" '+url
+            os.system(cmd+' >> /tmp/'+self.report+'.json')
+            codeql_f = open('/tmp/'+self.report+'.json', 'r', encoding="utf-8")
+            data = json.load(codeql_f)
+            codeql_f.close()
+
+        else:
+            try:
+                raw_data = urlopen(url)
+            except Exception as err:
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                print(exc_type, fname, exc_tb.tb_lineno)
+                sys.exit(str(err))
+            data = json.loads(raw_data.read())
+
+        return data
+
+    def write_report(self, data):
+        """
+            create report for criticality_score, codeql, and scorecard
+            in assesment data store
+        """
+        assessment_dir = os.environ['ASSESSMENT_DIR']
+
+        path = assessment_dir+'/'+self.name+'/'+self.version+'/'+self.report
+
+        try:
+            os.makedirs(path, exist_ok=True)
+        except FileExistsError as err:
+            print(f"error to create file Error: {err}")
+            
+        f = open(path + '/' + self.name + '-' + self.version +
+                 '-' + self.report + '-report.json', "w", encoding="utf-8")
+
+        f.write(json.dumps(data, indent=4))
+
+    def update_version_data(self):
+        """
+            provide versiondetails for version and read from assessment data store
+        """
+        osspoi_dir = os.environ['OSSPOI_DIR']
+        assessment_dir = os.environ['ASSESSMENT_DIR']
+        report_file = open(assessment_dir+'/'+self.name+'/'+self.version+'/'+self.report +
+                           '/'+self.name + '-' + self.version + '-' + self.report + '-report.json', "r", encoding="utf-8")
+        version_file = open(osspoi_dir+"/version_details/"+str(self.issue_id) +
+                            "-" + self.name + "-" "Versiondetails.json", "r+", encoding="utf-8")
+
+        score_data = json.load(report_file)
+        if self.report == "scorecard":
+            score = score_data["score"]
+        elif self.report == "criticality_score":
+            score = score_data["criticality_score"]
+
+        version_data = json.load(version_file)
+        for i, version in range(len(version_data)):
+            # fix me 
+            if version == self.version:
+                version_data[i][self.report] = score
+        version_file.seek(0)
+        version_file.write(json.dumps(version_data, indent=4))
+        version_file.truncate()
+        report_file.close()
+        version_file.close()
+
+    def cleanup(self):
+        """
+            remove file from tmp
+        """
+        if self.report != "scorecard":
+            os.remove('/tmp/'+self.report+'.json')
+
+    def main(self):
+        """
+            generate report for scorecard, criticality_score amd codeql
+        """
+        if self.report == "scorecard":
+            url = "https://api.securityscorecards.dev/projects/github.com/Be-Secure/"+self.name
+
+        elif self.report == "criticality_score":
+            if "GITHUB_AUTH_TOKEN" not in os.environ:
+                print(
+                    "[bold red] Alert! [green] Please use the below command to set your personal access token and try again.")
+                print("[yellow]export GITHUB_AUTH_TOKEN=token")
+                sys.exit()
+            url = 'github.com/Be-Secure/'+self.name
+
+        elif self.report == "codeql":
+            if "GITHUB_AUTH_TOKEN" not in os.environ:
+                print(
+                    "[bold red] Alert! [green] Please use the below command to set your personal access token and try again.")
+                print("[yellow]export GITHUB_AUTH_TOKEN=token")
+                sys.exit()
+            url = 'https://api.github.com/repos/Be-Secure/' + \
+                self.name + '/code-scanning/alerts?tool_name=CodeQL'
+
+        data = self.fetch_report(url)
+        self.write_report(data)
+        self.cleanup()
